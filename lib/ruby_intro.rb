14fe76abe603c6871a79c4f1c59fc0eb5dcfab26
EC2 Default User <ec2-user@ip-172-31-39-212.ec2.internal>
2018-09-13 02:05:26 +0000

    Finished parts 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,72 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  arr.each{|x| sum+=x}
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 1
+    return arr[0]
+  end
+  if arr.length == 0
+    return 0
+  end
+  sum = 0
+  sum = arr.max(2).reduce(:+)
+  return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # YOUR CODE HERE 
+  if arr.empty?
+    return false
+  end
+  if arr.length == 1
+    return false
+  end
+  !!arr.combination(2).find{|x,y| x+y == n} 
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, "+ name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  vowels = ["a","e","i","o","u","A","E","I","O","U"]
+  if s.empty?
+    return false
+  end
+  
+  if !/[A-Za-z]/.match(s[0])
+    return false
+  end
+  
+  for i in vowels do
+    if s[0] == i 
+      return false
+    end
+  end
+  return true
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if !/\A\d+\z/.match(s)
+    return false
+  end
+  num = s.to_i
+  if num % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
